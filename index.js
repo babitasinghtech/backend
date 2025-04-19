646e312ccb439a7c84c16fb95e167ecc21d2193b
Babita Singh <babitasinghtech@gmail.com>
2025-04-19 21:19:16 +0530

    "learnig the backend"

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.end("Babita ka Node server working! 🎉");
-});
-
-server.listen(3000, () => {
-  console.log("✅ Basic Node server running on http://localhost:3000");
-});
+const express = require(express)
+const aap = express();
+app.listen ()
